cc39fc5593c18dec5054148c9acf0c1134870927
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-09-20 11:16:31 +0200

    Reset base project

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -42,6 +42,5 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Produits', 'fas fa-car', Product::class);
     }
 }
