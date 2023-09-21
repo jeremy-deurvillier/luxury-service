b31b6cd83e9ca2e73cf4765702cb009c7183b5fe
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-09-21 09:30:31 +0200

    Update migrations and recreate database

--- migrations/Version20230921072505.php
+++ migrations/Version20230921072505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230920150022 extends AbstractMigration
+final class Version20230921072505 extends AbstractMigration
 {
     public function getDescription(): string
     {
